39368c979e90ab40a63877681471c9d344010df6
unknown <mandyatwork24@gmail.com>
2024-02-08 19:31:26 +0530

    alerts

---
+++ src/test/java/handlingUIElements/JavascriptAlert.java
@@ -0,0 +1,43 @@
+package handlingUIElements;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavascriptAlert {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+		
+		driver.findElement(By.xpath("//ul//li[1]/button")).click();
+		 Thread.sleep(2000);
+		 /**
+		  * For handling JS Alerts
+		  * We have to switch to Alert and we can either accept it or dismiss it
+		  * We can getText of alert as well
+		  */
+		Alert alert=  driver.switchTo().alert();
+		String alertText=alert.getText();
+		System.out.println(alertText);
+		
+		
+		 alert.accept();
+		
+		 driver.findElement(By.xpath("//ul/li[2]/button")).click();
+		 Thread.sleep(1000);
+		 alert.dismiss();
+		 
+		 driver.findElement(By.xpath("//ul/li[3]/button")).click();
+		 Thread.sleep(1000);
+		 alert.sendKeys("My name is NISSY");
+		 alert.accept();
+		 
+		 
+
+	}
+
+}
