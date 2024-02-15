77a58b20444bab2951c320783b79c8be8ecee72c
unknown <mandyatwork24@gmail.com>
2024-02-15 20:03:08 +0530

    tables

---
+++ src/test/java/handlingUIElements/HandleTables.java
@@ -0,0 +1,36 @@
+package handlingUIElements;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleTables {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://selenium.qabible.in/table-pagination.php");
+		
+		List<WebElement> list = driver.findElements(By.xpath("//table[@id='dtBasicExample']/tbody/tr[4]/td"));
+		
+
+		
+		for(int i =0;i<list.size();i++) {
+			System.out.println(list.get(i).getText());
+		}
+	
+		
+//		for(WebElement element : list) {
+//			System.out.println(element.getText());
+//		}
+		
+		driver.quit();
+		
+
+	}
+
+}
