dfe67859a1e2b467a7e6b4ddc0cf084e4b8c2d82
unknown <mandyatwork24@gmail.com>
2024-03-14 20:30:09 +0530

    upload code

---
+++ src/test/java/Java/LearnBasic.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class LearnBasic {
+
+	public static void main(String[] args) {
+		
+		String driver =null ;
+		driver= "mandy";
+		if(true) {
+			 driver= "remya";
+		}
+		
+		System.out.println(driver);
+
+	}
+
+}
---
+++ src/test/java/basic/UploadPicture.java
@@ -0,0 +1,43 @@
+package basic;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UploadPicture {
+
+	public static void main(String[] args) throws AWTException, InterruptedException {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.ilovepdf.com/pdf_to_word");
+		
+		driver.findElement(By.xpath("//*[contains(text(),'Select PDF file')]")).click();
+		Thread.sleep(2000);
+		String fileToUpload= "C:\\Users\\InfoBahn\\eclipse-workspace\\SeleniumAutomation\\Dogs.pdf";
+		StringSelection ss = new StringSelection(fileToUpload);
+	     Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+
+	     //imitate mouse events like ENTER, CTRL+C, CTRL+V
+	     Robot robot = new Robot();
+	     robot.delay(250);
+	     robot.keyPress(KeyEvent.VK_ENTER);
+	     robot.keyRelease(KeyEvent.VK_ENTER);
+	     robot.keyPress(KeyEvent.VK_CONTROL);
+	     robot.keyPress(KeyEvent.VK_V);
+	     robot.keyRelease(KeyEvent.VK_V);
+	     robot.keyRelease(KeyEvent.VK_CONTROL);
+	     robot.keyPress(KeyEvent.VK_ENTER);
+	     robot.delay(90);
+	     robot.keyRelease(KeyEvent.VK_ENTER);
+		
+
+	}
+
+}
